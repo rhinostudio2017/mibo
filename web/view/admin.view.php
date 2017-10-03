0dab3224d852aa5ab43ea243dccc14710c85c634
timsun <timsun@firmstep.com>
2017-10-04 03:20:23 +1300

    Page-favicon Add favicon

--- web/view/admin.view.php
+++ web/view/admin.view.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <link rel="shortcut icon" href="mibo48.png">
     <title>Mibo | Chinese Pink Movie</title>
 </head>
 <body>
